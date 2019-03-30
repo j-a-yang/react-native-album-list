841161195579190e97ea0c205406adefaf15dcce
ximedialabs101 <info@ximedialabs.com>
2019-03-30 14:35:14 -0400

    Displaying Album Artwork

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,69 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+// destructured the props object to just pass album
+const AlbumDetail = ({ album }) => {
+
+  // further destructuring the album property (prop)
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
-      </CardSection>  
+        <View style={thumbnailContainerStyle}>
+          <Image
+          style={thumbnailStyle}
+          source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image
+        style={imageStyle}
+        source={{ uri: image }}
+        />
+      </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+
 };
 
 export default AlbumDetail;
